49df9be9efec5beb7a10a369acd6e1273a1a19e2
audreykho <ackho@uw.edu>
2021-09-09 23:14:54 -0400

    nav + footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import { ReactComponent as LinkArrow } from "./assets/arrow-up-right.svg";
+import { Mail, LogoLinkedin, LogoGithub } from "react-ionicons";
+import styled from "styled-components";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavStyles>
+        <h3>AUDREY KHO</h3>
+        <div className="external">
+          <a
+            href="https://drive.google.com/file/d/1K4P0W8m9sU6cNN-XS0x6gC4wB7RACUpC/view"
+            target="_blank"
+            rel="noreferrer"
+          >
+            RESUME <LinkArrow />
+          </a>
+          <a href="mailto:audreyckho@gmail.com" target="_blank" rel="noreferrer">
+            <Mail color="#d8dbdb" />
+          </a>
+          <a href="https://www.linkedin.com/in/audrey-kho/" target="_blank" rel="noreferrer">
+            <LogoLinkedin color="#d8dbdb" />
+          </a>
+          <a href="https://github.com/audrey-kho" target="_blank" rel="noreferrer">
+            <LogoGithub color="#d8dbdb" />
+          </a>
+        </div>
+      </NavStyles>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <FooterStyles>
+        <h5>&copy; AUDREY KHO 2021</h5>
+      </FooterStyles>
+    </>
   );
 }
 
-export default App;
+const NavStyles = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  h3 {
+    font-weight: 400;
+    letter-spacing: 2px;
+  }
+
+  a {
+    color: #d8dbdb;
+    text-decoration: none;
+    display: flex;
+    align-items: flex-start;
+  }
+
+  .external {
+    display: flex;
+
+    > * + * {
+      margin-left: 18px;
+    }
+  }
+`;
+
+const FooterStyles = styled.footer`
+  position: absolute;
+  bottom: 0;
+
+  h5 {
+    letter-spacing: 2px;
+    font-weight: 500;
+  }
+`;
