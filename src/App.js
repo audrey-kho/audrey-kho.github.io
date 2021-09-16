e04632cd700bb6ab28489d3d7e4f6c02c8ae2542
audreykho <ackho@uw.edu>
2021-09-15 23:48:53 -0400

    dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import LinkArrow from "./assets/LinkArrow";
-import { Mail, LogoLinkedin, LogoGithub, ChevronDownSharp } from "react-ionicons";
+import { Mail, LogoLinkedin, LogoGithub, MenuOutline } from "react-ionicons";
 import About from "./components/About";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import Work from "./components/Work";
+import { useState, useEffect, useRef } from "react";
+import clsx from "clsx";
 
 export default function App() {
+  const [open, setOpen] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const checkIfClickedOutside = e => {
+      if (open && ref.current && !ref.current.contains(e.target)) {
+        setOpen(false)
+      }
+    }
+
+    document.addEventListener("mousedown", checkIfClickedOutside)
+
+    return () => {
+      document.removeEventListener("mousedown", checkIfClickedOutside)
+    }
+  }, [open])
+
   return (
     <>
       <NavStyles>
         <h2>AUDREY KHO</h2>
-        <div className="external">
+        <div className="main-menu">
           <a
             href="https://drive.google.com/file/d/1K4P0W8m9sU6cNN-XS0x6gC4wB7RACUpC/view"
             target="_blank"
             rel="noreferrer"
           >
             RESUME
-            <LinkArrow width={15} height={15} stroke={2}/>
+            <LinkArrow width={15} height={15} stroke={2} />
           </a>
           <a href="mailto:audreyckho@gmail.com" target="_blank" rel="noreferrer">
             <Mail color="#d8dbdb" width="26px" height="24px" />
@@ -28,18 +47,50 @@ export default function App() {
             <LogoGithub color="#d8dbdb" width="24px" height="24px" />
           </a>
         </div>
-        <a>
-          <ChevronDownSharp color="#d8dbdb" width="16px" height="16px"/>
-        </a>
+        <button className="mobile-menu" ref={ref}>
+          <MenuOutline color="#d8dbdb" width="22px" height="22px" onClick={() => {setOpen(!open)}}/>
+
+          <div className={clsx({"mobile-dropdown": true, "hidden": !open})}>
+            <a
+              href="https://drive.google.com/file/d/1K4P0W8m9sU6cNN-XS0x6gC4wB7RACUpC/view"
+              target="_blank"
+              rel="noreferrer"
+              onClick={() => {setOpen(false)}}
+            >
+              RESUME
+            </a>
+            <a href="mailto:audreyckho@gmail.com" target="_blank" rel="noreferrer" onClick={() => {setOpen(false)}}>
+              E-MAIL
+            </a>
+            <a href="https://www.linkedin.com/in/audrey-kho/" target="_blank" rel="noreferrer" onClick={() => {setOpen(false)}}>
+              LINKEDIN
+            </a>
+            <a href="https://github.com/audrey-kho" target="_blank" rel="noreferrer" onClick={() => {setOpen(false)}}>
+              GITHUB
+            </a>
+          </div>
+        </button>
       </NavStyles>
       <About />
-      <Work/>
+      <Work />
       <FooterStyles>
         <h5>&copy; AUDREY KHO 2021</h5>
       </FooterStyles>
     </>
   );
 }
+
+const dropdown = keyframes`
+  from {
+    transform: translateY(-6px);
+    opacity: 0.4;
+  }
+
+  to {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+`
 
 const NavStyles = styled.nav`
   display: flex;
@@ -56,7 +107,7 @@ const NavStyles = styled.nav`
 
   h2 {
     font-size: 16px;
-    margin: 30px 0;
+    margin: 24px 0;
   }
 
   a {
@@ -65,12 +116,68 @@ const NavStyles = styled.nav`
     align-items: flex-start;
   }
 
-  .external {
+  .main-menu {
     display: flex;
     align-items: center;
 
     > * + * {
       margin-left: 22px;
+    }
+
+    @media (max-width: 768px) {
+      display: none;
+    }
+  }
+
+  .mobile-menu {
+    position: relative;
+    display: none;
+    background: none;
+    border: none;
+    padding: 0;
+    border-radius: 3px;
+    transition: 0.3s all;
+
+    svg {
+      padding: 4px 8px 2px;
+    }
+
+    .mobile-dropdown {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      font-family: "Whyte", sans-serif;
+      letter-spacing: 1px;
+      position: absolute;
+      top: 50px;
+      right: 0;
+      padding: 6px 16px;
+      background: rgba(22, 38, 66, 0.5);
+      backdrop-filter: blur(4px);
+      box-shadow: 0 0 2px rgba(102, 118, 146, 0.8);
+      border-radius: 6px;
+      pointer-events: none;
+      animation: ${dropdown} 0.2s ease;
+
+      a {
+        margin: 4px 0;
+        padding: 2px;
+        cursor: pointer !important;
+        pointer-events: auto;
+      }
+
+      &.hidden {
+        display: none;
+      }
+    }
+
+    &:hover:not(.mobile-dropdown), &:active, &:focus {
+      box-shadow: 0 0 2px rgba(180, 180, 220, 0.8);
+      // transform: translateY(-1px);
+    }
+
+    @media (max-width: 768px) {
+      display: block;
     }
   }
 `;
