a0cf0e5b8a461a15392d8520931532c403e2c29c
Audrey Kho <audreyckho@gmail.com>
2022-03-19 05:08:00 -0700

    swap colors

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -147,7 +147,7 @@ const ContactStyles = styled.section`
     font-family: "Whyte Medium", sans-serif;
     display: inline;
     height: 46px;
-    color: #0c1831;
+    color: #3f112d;
     font-size: 0.86em;
     font-weight: 600;
     letter-spacing: 1.5px;
