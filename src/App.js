3e10d62cd9851059c3ec4cfaf81008a588db8817
Audrey Kho <audreyckho@gmail.com>
2022-05-14 23:04:04 -0700

    color chnage

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ export default function App() {
 
   return (
     <Suspense fallback={
-      <div style={{width: "100%", height: "80em", background: "#3b5144"}}></div>
+      <div style={{width: "100%", height: "80em", background: "#3c4e41"}}></div>
     }>
       <NavStyles>
         <h2>AUDREY KHO</h2>
