71f8864efae8a3e8c2825e0951cfc55ae0137498
Audrey Kho <akho@Audreys-MBP-2.lan>
2023-08-24 08:59:40 -0700

    Updates

---
+++ assets/ResponsiveContainer-1483d8d0.js
@@ -0,0 +1,45 @@
+import{j as t,N as a,O as e,s as i}from"./index-ce1bf2b0.js";const m=(...n)=>{const{href:o,children:s}=n[0];return t.jsx(r,{children:t.jsxs(a,{to:o,smooth:!0,offset:-92,children:[s," ",t.jsx(e,{color:"#485A3D",width:"10px",height:"10px"})]})})},r=i.p`
+  display: flex;
+  position: sticky;
+  top: 80px;
+  margin: 1px 0 0 !important;
+  font-family: "DM Mono", monospace;
+  font-size: 13px;
+  font-weight: 400;
+  min-width: 120px;
+  letter-spacing: 1px;
+
+  svg {
+    transition: 0.3s all;
+  }
+
+  a:hover svg {
+    transform: translateX(3px);
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
+`,c=i.h2`
+  font-family: "DM Mono", monospace;
+  font-size: 15px;
+  font-weight: 500;
+  margin: 0 0 14px;
+  letter-spacing: 1.5px;
+`,d=n=>t.jsx(p,{children:n.children}),p=i.section`
+  margin-left: 60px;
+  width: 100%;
+
+  @media (max-width: 768px) {
+    margin: 0;
+  }
+`,l=i.div`
+  position: relative;
+  max-width: 1208px;
+  margin: 0 auto;
+  padding: 0 5px;
+
+  @media (max-width: 1280px) {
+    margin: 0 16px;
+  }
+`;export{d as M,l as R,m as S,c as a};
